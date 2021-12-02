4a420c4360ad92b0dbbce8bdc1556fd464fab0c7
Jordan <jstrande@rams.colostate.edu>
2021-12-02 12:29:08 -0700

    13.1 12/1 add page components, routing

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>Not Found</h1>
+      <h4>Sorry, This page could not be found</h4>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
